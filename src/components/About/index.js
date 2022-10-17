7f5c2b1802d8729fab9e5f294c5659bac2b988c7
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-10-17 19:52:59 -0400

    created src files for rendering

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import coverImage from "../../assets/cover/profile.jpg";
+import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
 
   return (
     <section className="my-5" >
       <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "40%" }} alt="cover" />
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="my-2">
         <p>
-        I'm an aspiring web developer with ten years of administrative experience in the Federal Government.
-        </p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+      </p>
       </div>
     </section>
   )
