e5d05ccea406d7c015d116b6e42ef4d88bee99c5
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-10-17 20:14:46 -0400

    completed About index

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,40 @@
-import React from 'react'
-import coverImage from "../../assets/cover/cover-image.jpg";
+import React from 'react';
 
 function About() {
-
-  return (
-    <section className="my-5" >
-      <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-      </p>
-      </div>
-    </section>
-  )
+	return (
+		<section>
+			<div className="center" id="about">
+				<h1 className="page-header">Who Am I?</h1>
+			</div>
+			<div className="center">
+				<img
+					src={require('../../assets/images/profile.jpg')}
+					alt="about-me"
+					className="photo"
+				/>
+			</div>
+			<div>
+				<p>
+          I'm an aspiring web developer with ten years of 
+          administrative experience in the Federal Government.
+          I have a certification in Business Data Analysis from
+          Cornell University and I am currently working on a Web
+          Development Coding Bootcamp with Carleton
+          University. I have ten years of administrative 
+          experience with the Federal Government in an an 
+          Emergency Management and Business Continuity Officer 
+          role.
+          
+				</p>
+				<p>
+          I am searching for an opportunity as a full-stack web 
+          developer where I can use my recently obtained skills 
+          to bring positive change to a company that I deem a 
+          good fit for my values and skillset.
+				</p>
+			</div>
+		</section>
+	);
 }
 
-export default About
+export default About;
